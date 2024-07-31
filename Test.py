ad4e5e5618f28b08d30ef26ad7d9be4d95608c9b
Mohsen Amiri <mohsen.amiri@wsu.edu>
2024-07-31 12:57:05 -0700

    Update

--- Test.py
+++ Test.py
@@ -8,7 +8,7 @@ import cvxpy as cp
 #------------------------------------------
 # Robot Model
 n = 100
-Prediction_Horizon = 5.0
+Prediction_Horizon = 5
 deltaT=0.5
 
 A_R = np.array([1.0])
@@ -19,14 +19,15 @@ D_R = np.zeros((1, 1))
 NoI_R=B_R.shape[1]
 NoS_R=A_R.shape[0]
 NoO_R=C_R.shape[0]
-Abar = np.array([A_R])
 
+#------------------------------------------------------------------------------------
+## Robot Predictive model
+Abar = A_R
 if A_R.shape[0]==1:
     for i in range(2, Prediction_Horizon + 1):
         Abar = np.vstack((Abar, A_R**i))
 
     Bbar = np.zeros((NoS_R * Prediction_Horizon, NoI_R * Prediction_Horizon))
-
 # Loop to fill Bbar with the appropriate blocks
     for i in range(1, Prediction_Horizon + 1):
       for j in range(1, i + 1):
@@ -49,7 +50,8 @@ else:
     for i in range(1, Prediction_Horizon + 1):
         for j in range(1, i + 1):
             Bbar[(i-1)*NoS_R:i*NoS_R, (j-1)*NoI_R:j*NoI_R] = np.linalg.matrix_power(A_R, i-j) @ B_R
-#------------------------------------------
+#------------------------------------------------------------------------------------
+
 # print(Bbar)
 # Human Model
 A_H = np.array([1.0])
@@ -60,6 +62,37 @@ D_H = np.zeros((1, 1))
 NoI_H=B_H.shape[1]
 NoS_H=A_H.shape[0]
 NoO_H=C_H.shape[0]
+
+## Human Predictive model
+Abar_H = A_H
+if A_H.shape[0]==1:
+    for i in range(2, Prediction_Horizon + 1):
+        Abar_H = np.vstack((Abar_H, A_H**i))
+
+    Bbar_H = np.zeros((NoS_H * Prediction_Horizon, NoI_H * Prediction_Horizon))
+# Loop to fill Bbar with the appropriate blocks
+    for i in range(1, Prediction_Horizon + 1):
+      for j in range(1, i + 1):
+        # Compute A_H^(i-j)
+        A_power = A_H ** (i - j)
+        
+        # Compute the block (A_power * B_H), since B_H is scalar we multiply directly
+        block = A_power * B_H
+
+        # Calculate the indices for insertion
+        row_indices = slice((i - 1) * NoS_H, i * NoS_H)
+        col_indices = slice((j - 1) * NoI_H, j * NoI_H)
+
+        # Insert the block into the appropriate position in Bbar
+        Bbar_H[row_indices, col_indices] = block
+else:
+    Abar_H = np.vstack([np.linalg.matrix_power(A_H, i) for i in range(1, Prediction_Horizon+1)])
+    Bbar_H = np.zeros((NoS_H * Prediction_Horizon, NoI_H * Prediction_Horizon))
+
+    for i in range(1, Prediction_Horizon + 1):
+        for j in range(1, i + 1):
+            Bbar_H[(i-1)*NoS_H:i*NoS_H, (j-1)*NoI_H:j*NoI_H] = np.linalg.matrix_power(A_H, i-j) @ B_H
+#------------------------------------------------------------------------------------
 #------------------------------------------
 ## Contorller
 #INPUTS=x_H ,X_R
@@ -74,9 +107,9 @@ P_x_H=-5
 # print(betas)
 
 # x_H = -5.0*np.ones((NoS_H,n))
-# Nc=np.array([-5.0,-4.5,-4.0,-3.5,-3.0,-2.5,-2.0,-1.5,-1.0,-0.5,0.0,0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5]).reshape(-1,1)   
+Nc=np.array([-5.0,-4.5,-4.0,-3.5,-3.0,-2.5,-2.0,-1.5,-1.0,-0.5,0.0,0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5]).reshape(-1,1)   
 x_H = 0.0*np.ones((NoS_H,n))
-Nc=np.array([0.0,0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5]).reshape(-1,1)   
+# Nc=np.array([0.0,0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5]).reshape(-1,1)   
 
 g_H = np.array([5.0]).reshape(-1,1)  
 g_R = np.array([80.0]).reshape(-1,1) 
@@ -84,6 +117,7 @@ g_R_pr=np.tile(g_R, Prediction_Horizon)
 v_R = np.array([2.0]).reshape(-1,1)  
 v_h = np.array([0.5]).reshape(-1,1)  
 w_H = np.array([0.5]).reshape(-1,1)  
+u_H_values = np.array([-2, -1, 0, 1, 2]).reshape(-1,1)
 
 P_th = np.array([0.1]).reshape(-1,1)  
 T_R = np.array([5.0]).reshape(-1,1)  
@@ -124,7 +158,7 @@ def Human_Action_Prediction(u_H,u_H_values,w_H,gamma,beta,betas,x_H0,hat_x_R,g_H
    P_r=1/U_H
   
    P_u_H=(1-w_H) * P_d+w_H * P_r
-   print(P_u_H)
+#    print(P_u_H)
    return P_u_H
 #    print(P_r)
 
@@ -156,7 +190,7 @@ def  human_s_action(NoI_H,u_H_values,x_H0,g_H,theta_3,theta_4,theta_5,theta_6,ha
         if problem.status != cp.OPTIMAL:
             flag += 1
         sss=u_H.value
-        print(sss)
+        # print(sss)
         return sss
 # Robot’s Belief About the Human’s Danger Awareness
 def Robot_s_Belief_About_HDA(u_H,u_H_values,w_H,gamma,beta,betas,P_t,P_ts):
@@ -190,16 +224,25 @@ def Probability_distribution_of_human_s_states(u_H,w_H,gamma,beta,betas,P_t,P_ts
     P_u_H=np.zeros((u_H_values.shape[0]*betas.shape[0])).reshape(-1,1)
     # fff=u_H_values.shape[0]*betas.shape[0]
     sum_x_H=0.0
-    P=np.zeros((Nc.shape[0],1))
+    P=np.zeros((Prediction_Horizon,Nc.shape[0],1))
+    # P[0,:,:]=1.0
     P_x_H=np.zeros((Nc.shape[0],1))
     P_x_H_ik=np.zeros((Nc.shape[0],u_H_values.shape[0]*betas.shape[0]))
-    for j in range(Prediction_Horizon):
+    x_H_next_p=np.zeros((Prediction_Horizon,1))
+    P_P=np.zeros((Prediction_Horizon,Nc.shape[0]))
+    # u_H_values_flat = u_H_values.flatten()
+    # u_H_values = np.tile(u_H_values_flat, Prediction_Horizon)
+    # for tt in range(Prediction_Horizon):
+    #     u_H_values_P=
+
+    for j in range(Prediction_Horizon-1):
 
         for m in range(Nc.shape[0]):
 
             for k in range(u_H_values.shape[0]):
                 x_H_next=A_H*x_H0+B_H*u_H_values[k]
-                x_H_next = Abar_H @ x_H0 + Bbar_H @ u_R
+
+                # x_H_next_p = Abar_H @ x_H0 + Bbar_H @ (u_H_values[k]*np.ones((Prediction_Horizon,1)))
                 
                 if np.allclose(x_H_next, Nc[m, 0], atol=tolerance):
                     P_x_H_k=1.0
@@ -213,24 +256,35 @@ def Probability_distribution_of_human_s_states(u_H,w_H,gamma,beta,betas,P_t,P_ts
 
             for k in range(u_H_values.shape[0]): 
 
-                x_H_next=A_H*x_H+B_H*u_H_values[k]
+                x_H_next=A_H*x_H0+B_H*u_H_values[k]
+
+                # x_H_next_p = Abar_H @ x_H0 + Bbar_H @ (u_H_values[k]*np.ones((Prediction_Horizon,1)))
+                
                 if np.allclose(x_H_next, Nc[m, 0], atol=tolerance):
                     P_x_H_k=1.0
                 else:
-                    P_x_H_k=0.0          
+                    P_x_H_k=0.0         
 
                 for i in range(betas.shape[0]):
 
-                    print(u_H_values[k,0])
+                    # print(u_H_values[k,0])
                     P_u_H=Human_Action_Prediction(u_H_values[k,0],u_H_values,w_H,gamma,betas[i],betas,x_H0,hat_x_R,g_H,theta_3,theta_4,theta_5,theta_6)
                     P_t, P_ts=Robot_s_Belief_About_HDA(u_H,u_H_values,w_H,gamma,beta,betas,P_t,P_ts)   
-                    P_x_H_ik[m,i+k]=(P_x_H_k*P_u_H*P_t)  
+                    P_x_H_ik[m,i+k]=(P_x_H_k*P_u_H*P_t) 
+                 
+         
+          
+            P_x_H[m,:]=np.sum(P_x_H_ik[m,:]) 
+            # print(P_x_H)   
 
-            P_x_H[m,0]=np.sum(P_x_H_ik[m,i+k])    
+        if j==0:
+            P[j,:,:]= P_x_H/(sum_x_H)  
+            print(P[0,:,:])
+        else:   
 
-            
-        P[m,0]= P_x_H[m,0]/(sum_x_H)  
-    
+
+            P[j+1,m,0]=P[j,m,0]* P_x_H[m,0]/(sum_x_H)  
+    # P_P[j,m]= P[m,0]
     return P
 
 # Probability of Collision
@@ -245,7 +299,7 @@ u_app_R = np.zeros((NoI_R, n))
 for i in range(n):
      
     #Updates
-    x_H0=x_H[:, i]
+    x_H0=x_H[:, i].reshape(-1,1)
     x_R0=x_R[:, i].reshape(-1,1)
     # Generate zero-mean Gaussian noise
     epsilon = np.random.normal(mean, std_deviation, num_samples)
@@ -256,7 +310,7 @@ for i in range(n):
 
 
 
-    u_H_values = np.array([-2, -1, 0, 1, 2]).reshape(-1,1)
+    
 
 
     
