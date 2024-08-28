04bcaed4817c004bdd7171ccd909e4f629b4ce9b
Mohsen Amiri <mohsen.amiri@wsu.edu>
2024-08-28 11:41:53 -0700

    8282024

--- 2D/2DmodelPredictive.py
+++ 2D/2DmodelPredictive.py
@@ -137,7 +137,7 @@ P_t=np.array([.5,
                 .5])
 
 Nc=np.array([-5.0,-4.5,-4.0,-3.5,-3.0,-2.5,-2.0,-1.5,-1.0,-0.5,0.0,0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0])
-Nc=np.array([-3.0,-2.5,-2.0,-1.5,-1.0,-0.5,0.0,0.5,1.0,1.5,2.0,2.5,3.0])
+# Nc=np.array([-3.0,-2.5,-2.0,-1.5,-1.0,-0.5,0.0,0.5,1.0,1.5,2.0,2.5,3.0])
 # Create a meshgrid for X and Y coordinates
 # Create a meshgrid for X and Y coordinates
 X, Y = np.meshgrid(Nc, Nc)
